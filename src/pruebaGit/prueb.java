415b7958adcdfcdd1dbdcf0c91bd7bc2188f3c8e
Nuria Muñoz <nuriatoboso@gmail.com>
2023-06-09 19:23:30 +0200

    modificacion2

--- src/pruebaGit/prueb.java
+++ src/pruebaGit/prueb.java
@@ -5,6 +5,7 @@ public class prueb {
 	public static void main(String[] args) {
 		System.out.println("Nuria Muñoz");
 		System.out.println("Modificacion desde master");
+		System.out.println("modificacion desde develop");
 	}
 
 }
